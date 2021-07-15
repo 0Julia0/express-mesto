8835b67731b910f20bd1dd7a05b529da68b50aab
Julia Pinkevich <yuliapinkevitch@yandex.by>
2021-07-15 14:29:09 +0300

    test

---
+++ app.js
@@ -0,0 +1,95 @@
+require('dotenv').config();
+const express = require('express');
+const mongoose = require('mongoose');
+const { errors } = require('celebrate');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const { celebrate, Joi } = require('celebrate');
+const helmet = require('helmet');
+const cors = require('cors');
+const usersRouter = require('./routes/users');
+const cardsRouter = require('./routes/cards');
+const { createUser, login } = require('./controllers/users');
+const auth = require('./middlewares/auth');
+const { requestLogger, errorLogger } = require('./middlewares/logger');
+
+/*
+const { PORT = 3000 } = process.env;
+*/
+
+const whitelist = [
+  'http://localhost:3000',
+  'https://localhost:3000',
+  'http://localhost:2000',
+  'https://localhost:2000',
+  'https://julia.p.nomoredomains.club',
+  'http://julia.p.nomoredomains.club',
+  'https://api.julia.p.nomoredomains.monster',
+  'http://api.julia.p.nomoredomains.monster',
+];
+const corsOption = {
+  credentials: true,
+  origin: function checkCorsList(origin, callback) {
+    if (whitelist.indexOf(origin) !== -1 || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+};
+
+const app = express();
+
+app.use(helmet());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.use(cors(corsOption));
+
+mongoose.connect('mongodb://localhost:27017/mestodb', {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+  useUnifiedTopology: true,
+});
+
+app.use(cookieParser());
+
+app.use(requestLogger);
+
+app.get('/crash-test', () => {
+  setTimeout(() => {
+    throw new Error('Сервер сейчас упадёт');
+  }, 0);
+});
+
+app.post('/signin', celebrate({
+  body: Joi.object().keys({
+    email: Joi.string().required().email(),
+    password: Joi.string().required(),
+  }),
+}), login);
+app.post('/signup', celebrate({
+  body: Joi.object().keys({
+    email: Joi.string().required().email(),
+    password: Joi.string().required(),
+  }),
+}), createUser);
+
+app.use(auth);
+
+app.use('/users', usersRouter);
+app.use('/cards', cardsRouter);
+app.use('*', (req, res) => {
+  res.status(404).send({ message: 'Запрашиваемый ресурс не найден.' });
+});
+
+app.use(errorLogger);
+app.use(errors());
+
+app.use((err, req, res, next) => {
+  res.status(err.statusCode).send({ message: err.message });
+  next();
+});
+
+app.listen(2000);
